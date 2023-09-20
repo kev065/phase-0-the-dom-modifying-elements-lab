431e64432e4082cd25597bb3d9e8ef6df5e6e939
KELVIN MURITHI <kelvinmurithi@live.com>
2023-09-21 00:23:43 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+const YOUR_NAME = "kelvin";
+
+const main = document.querySelector("main#main");
+if (main) {
+  main.remove();
+}
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = `${YOUR_NAME} is the champion`;
